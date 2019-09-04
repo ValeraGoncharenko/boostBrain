5838e965db8d1993353c4175ccc2fab0a4064c7d
ValeraGoncharenko <goncharikvv@gmail.com>
2019-09-04 03:00:10 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+
+class Main {
+    
+    public static void main(String[] args) {
+
+        ReadingFile readingFile = new ReadingFile();
+        Main readingData = new Main();
+        readingFile.parseDataFromCsvFile();
+
+
+
+
+
+    }
+
+
+}
